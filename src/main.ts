83eefb9725098223ef0da484e95a06a8a1ce6f1b
Siddhant Sancheti <sidsanc4998@gmail.com>
2025-02-22 12:20:29 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,50 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import { fetchPosts } from './hashnodeQuery'
+import fs from 'fs'
+import commitFile from './commitFiles'
+import { getFormattedContent } from './utils/formatUtils'
+import { ContentFormat } from 'HashNodeTypes'
+
+const SECTION_REGEX =
+  /^(<!--(?:\s|)HASHNODE_BLOG:(?:START|start)(?:\s|)-->)(?:\n|)([\s\S]*?)(?:\n|)(<!--(?:\s|)HASHNODE_BLOG:(?:END|end)(?:\s|)-->)$/gm
 
 /**
  * The main function for the action.
- *
- * @returns Resolves when the action is complete.
+ * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const publicationName: string = core.getInput('HASHNODE_PUBLICATION_NAME')
+    const postCount: number = parseInt(core.getInput('POST_COUNT'))
+    const outputFileName: string = core.getInput('FILE')
+    const format: ContentFormat = (core.getInput('FORMAT') ??
+      'table') as ContentFormat
+    const isDebug: boolean = core.getInput('DEBUG') === 'true'
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    // fetch posts from hashnode
+    const response = await fetchPosts(publicationName, postCount)
+    const posts = response.data.publication.posts.edges.map(edge => edge.node)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const filePath = `${process.env.GITHUB_WORKSPACE}/${outputFileName}`
+    const fileContent = fs.readFileSync(filePath, 'utf8')
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const output = getFormattedContent(posts, format)
+
+    const result = fileContent
+      .toString()
+      .replace(SECTION_REGEX, `$1\n${output}\n$3`)
+
+    fs.writeFileSync(filePath, result, 'utf8')
+
+    // commit changes to the file when not in debug mode
+    if (!isDebug) {
+      // eslint-disable-next-line github/no-then
+      await commitFile().catch(err => {
+        core.error(err)
+        core.info(err.stack)
+        process.exit(err.code || -1)
+      })
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
